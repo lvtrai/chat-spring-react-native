ecea732fb983287f96a2e225c1c98db66f108c55
lvtrai <52622764+lvtrai@users.noreply.github.com>
2020-09-29 10:10:25 +0700

    setup

---
+++ src/main/java/com/lvtrai18/chatspringreactnative/ChatSpringReactNativeApplication.java
@@ -0,0 +1,13 @@
+package com.lvtrai18.chatspringreactnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSpringReactNativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatSpringReactNativeApplication.class, args);
+	}
+
+}
